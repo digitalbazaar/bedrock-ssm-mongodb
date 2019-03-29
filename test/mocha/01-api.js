6834c0474883dfdb9a149948c69016411902765f
Dave Longley <dlongley@digitalbazaar.com>
2019-03-28 22:14:29 -0400

    Use new KMS operation data model.

--- test/mocha/01-api.js
+++ test/mocha/01-api.js
@@ -6,19 +6,25 @@
 const brSSM = require('bedrock-ssm-mongodb');
 
 describe('generateKey API', () => {
-  it('returns a key id for type HS256', async () => {
+  it('returns a key id for type Sha256HmacKey2019', async () => {
+    const keyId = 'https://example.com/ksm/ssm-v1/my-hmac-key';
     const controller = 'https://example.com/i/foo';
-    const type = 'HS256';
-    const result = await brSSM.generateKey({controller, type});
+    const type = 'Sha256HmacKey2019';
+    const invocationTarget = {id: keyId, type, controller};
+    const result = await brSSM.generateKey(
+      {keyId, operation: {invocationTarget}});
     should.exist(result);
     result.should.be.an('object');
     result.should.have.property('id');
     result.id.should.be.a('string');
   });
-  it('returns a key id for type AES-KW', async () => {
+  it('returns a key id for type AesKeyWrappingKey2019', async () => {
+    const keyId = 'https://example.com/ksm/ssm-v1/my-kek';
     const controller = 'https://example.com/i/foo';
-    const type = 'AES-KW';
-    const result = await brSSM.generateKey({controller, type});
+    const type = 'AesKeyWrappingKey2019';
+    const invocationTarget = {id: keyId, type, controller};
+    const result = await brSSM.generateKey(
+      {keyId, operation: {invocationTarget}});
     should.exist(result);
     result.should.be.an('object');
     result.should.have.property('id');
