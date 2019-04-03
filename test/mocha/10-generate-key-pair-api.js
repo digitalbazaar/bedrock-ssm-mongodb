698a53375089a688e210357dd63c85d3116433df
Matthew Collier <mcollier@digitalbazaar.com>
2019-04-03 12:48:23 -0400

    Add generateKeyPair API.

---
+++ test/mocha/10-generate-key-pair-api.js
@@ -0,0 +1,25 @@
+/*!
+ * Copyright (c) 2019 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+const brSSM = require('bedrock-ssm-mongodb');
+const {util: {uuid}} = require('bedrock');
+
+describe('generateKeyPair API', () => {
+  it('returns a key id', async () => {
+    // uuid will be generated at the bedrock-kms-http layer
+    const keyId = `https://example.com/ksm/ssm-v1/${uuid()}`;
+    const controller = 'https://example.com/i/foo';
+    const type = 'Ed25519VerificationKey2018';
+    const invocationTarget = {id: keyId, type, controller};
+    const result = await brSSM.generateKeyPair(
+      {keyId, operation: {invocationTarget}});
+    should.exist(result);
+    result.should.be.an('object');
+    Object.keys(result).should.have.same.members(
+      ['id', 'publicKeyBase58', 'type']);
+    result.id.should.equal(keyId);
+    result.type.should.equal(type);
+  });
+});
