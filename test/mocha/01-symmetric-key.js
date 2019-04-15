4d4594a113c36010fbbc1d22171922e49ac44c48
Matthew Collier <mcollier@digitalbazaar.com>
2019-04-15 17:39:09 -0400

    Add describe block in test suite.

--- test/mocha/01-symmetric-key.js
+++ test/mocha/01-symmetric-key.js
@@ -5,29 +5,31 @@
 
 const brSSM = require('bedrock-ssm-mongodb');
 
-describe('generateKey API', () => {
-  it('returns a key id for type Sha256HmacKey2019', async () => {
-    const keyId = 'https://example.com/ksm/ssm-v1/my-hmac-key';
-    const controller = 'https://example.com/i/foo';
-    const type = 'Sha256HmacKey2019';
-    const invocationTarget = {id: keyId, type, controller};
-    const result = await brSSM.generateKey(
-      {keyId, operation: {invocationTarget}});
-    should.exist(result);
-    result.should.be.an('object');
-    result.should.have.property('id');
-    result.id.should.be.a('string');
-  });
-  it('returns a key id for type AesKeyWrappingKey2019', async () => {
-    const keyId = 'https://example.com/ksm/ssm-v1/my-kek';
-    const controller = 'https://example.com/i/foo';
-    const type = 'AesKeyWrappingKey2019';
-    const invocationTarget = {id: keyId, type, controller};
-    const result = await brSSM.generateKey(
-      {keyId, operation: {invocationTarget}});
-    should.exist(result);
-    result.should.be.an('object');
-    result.should.have.property('id');
-    result.id.should.be.a('string');
+describe('symmetric keys', () => {
+  describe('generateKey API', () => {
+    it('returns a key id for type Sha256HmacKey2019', async () => {
+      const keyId = 'https://example.com/ksm/ssm-v1/my-hmac-key';
+      const controller = 'https://example.com/i/foo';
+      const type = 'Sha256HmacKey2019';
+      const invocationTarget = {id: keyId, type, controller};
+      const result = await brSSM.generateKey(
+        {keyId, operation: {invocationTarget}});
+      should.exist(result);
+      result.should.be.an('object');
+      result.should.have.property('id');
+      result.id.should.be.a('string');
+    });
+    it('returns a key id for type AesKeyWrappingKey2019', async () => {
+      const keyId = 'https://example.com/ksm/ssm-v1/my-kek';
+      const controller = 'https://example.com/i/foo';
+      const type = 'AesKeyWrappingKey2019';
+      const invocationTarget = {id: keyId, type, controller};
+      const result = await brSSM.generateKey(
+        {keyId, operation: {invocationTarget}});
+      should.exist(result);
+      result.should.be.an('object');
+      result.should.have.property('id');
+      result.id.should.be.a('string');
+    });
   });
 });
