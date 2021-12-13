5ae77e8e228f46b3d5250ab5a9a8ddcbfde4b048
Josh Dunn <jdunn@digitalbazaar.com>
2021-12-13 16:46:19 -0700

    Add additional tests for better coverage.

--- test/mocha/10-asymmetric-key.js
+++ test/mocha/10-asymmetric-key.js
@@ -19,10 +19,13 @@ cryptoLd.use(Ed25519VerificationKey2020);
 cryptoLd.use(Ed25519VerificationKey2018);
 describe('asymmetric keys', () => {
   describe('Ed25519VerificationKey2018', () => {
+    let keyId;
+    before(async () => {
+      keyId = `https://example.com/kms/${await generateId()}`;
+    });
     describe('generateKey API', () => {
       it('successfully generates a key pair', async () => {
         // keyId will be generated at the bedrock-kms-http layer
-        const keyId = `https://example.com/kms/${await generateId()}`;
         const controller = 'https://example.com/i/foo';
         const type = 'Ed25519VerificationKey2018';
         const invocationTarget = {id: keyId, type, controller};
@@ -34,6 +37,30 @@ describe('asymmetric keys', () => {
         result.id.should.equal(keyId);
         result.type.should.equal(type);
       });
+      it('throws a DuplicateError when generating the same key twice',
+        async () => {
+          // keyId will be generated at the bedrock-kms-http layer
+          const keyId = `https://example.com/kms/${await generateId()}`;
+          const controller = 'https://example.com/i/foo';
+          const type = 'Ed25519VerificationKey2018';
+          const invocationTarget = {id: keyId, type, controller};
+          await brSSM.generateKey(
+            {keyId, operation: {invocationTarget}});
+
+          let result;
+          let err;
+          try {
+            result = await brSSM.generateKey(
+              {keyId, operation: {invocationTarget}});
+            should.exist(result);
+          } catch(e) {
+            err = e;
+          }
+
+          should.exist(err);
+          should.not.exist(result);
+          err.name.should.equal('DuplicateError');
+        });
     }); // end generateKey API
 
     describe('sign API', () => {
@@ -68,6 +95,138 @@ describe('asymmetric keys', () => {
         valid.should.be.true;
       });
     }); // end sign API
+
+    describe('wrapKey API', () => {
+      it('throws an Error when trying to wrap a key',
+        async () => {
+          const unwrappedKey = '8vEgpnq8F6QVRmaSYPHTKKZyCXMOgRLiBdZPcfYnIfI';
+
+          let result;
+          let err;
+          try {
+            result = await brSSM.wrapKey(
+              {keyId, operation: {unwrappedKey}});
+          } catch(e) {
+            err = e;
+          }
+
+          should.exist(err);
+          should.not.exist(result);
+          err.name.should.equal('Error');
+        });
+    });
+
+    describe('unwrapKey API', () => {
+      it('throws an Error when trying to unwrap a key',
+        async () => {
+          const wrappedKey = '8vEgpnq8F6QVRmaSYPHTKKZyCXMOgRLiBdZPcfYnIfI';
+
+          let result;
+          let err;
+          try {
+            result = await brSSM.unwrapKey(
+              {keyId, operation: {wrappedKey}});
+          } catch(e) {
+            err = e;
+          }
+
+          should.exist(err);
+          should.not.exist(result);
+          err.name.should.equal('Error');
+        });
+    });
+
+    describe('verify API', () => {
+      it('throws an Error when trying to verify',
+        async () => {
+          const plaintextBuffer = Buffer.from(uuid(), 'utf8');
+          const verifyData = base64url.encode(plaintextBuffer);
+          const operation = {
+            verifyData
+          };
+          const result = await brSSM.sign({keyId, operation});
+          const {signatureValue} = result;
+
+          let result2;
+          let err;
+          try {
+            result2 = await brSSM.verify(
+              {keyId, operation: {signatureValue, verifyData}});
+          } catch(e) {
+            err = e;
+          }
+
+          should.exist(err);
+          should.not.exist(result2);
+          err.name.should.equal('Error');
+        });
+    });
+
+    describe('deriveSecret API', () => {
+      it('throws an Error when trying to derive a secret', async () => {
+        const keyId = `https://example.com/kms/${await generateId()}`;
+        const controller = 'https://example.com/i/foo';
+        const type = 'Ed25519VerificationKey2018';
+        const invocationTarget = {id: keyId, type, controller};
+        await brSSM.generateKey(
+          {keyId, operation: {invocationTarget}});
+
+        let result;
+        let err;
+        try {
+          result = await brSSM.deriveSecret(
+            {keyId, operation: {invocationTarget}});
+        } catch(e) {
+          err = e;
+        }
+
+        should.exist(err);
+        should.not.exist(result);
+        err.name.should.equal('Error');
+      });
+      it('throws an Error if public and key type do not match inside ' +
+      '_keyAgreementKey.deriveSecret() helper function', async () => {
+        const key = {
+          type: 'typeA'
+        };
+        const publicKey = {
+          type: 'typeB'
+        };
+
+        let result;
+        let err;
+        try {
+          result = await brSSM._helpers._keyAgreementKey.deriveSecret(
+            {key, operation: {publicKey}});
+        } catch(e) {
+          err = e;
+        }
+
+        should.exist(err);
+        should.not.exist(result);
+        err.name.should.equal('Error');
+      });
+    });
+
+    describe('generateKeyPair API', () => {
+      it('throws an Error if invalid type is used inside ' +
+      '_keyAgreementKey.generateKeyPair() helper function', async () => {
+        const type = 'invalid';
+
+        let result;
+        let err;
+        try {
+          result = await brSSM._helpers._keyAgreementKey.generateKeyPair(
+            {keyId, type});
+        } catch(e) {
+          err = e;
+        }
+
+        should.exist(err);
+        should.not.exist(result);
+        err.name.should.equal('Error');
+      });
+    });
   }); // end Ed25519VerificationKey2018
   describe('Ed25519VerificationKey2020', () => {
     describe('generateKey API', () => {
@@ -123,10 +282,13 @@ describe('asymmetric keys', () => {
   }); // end Ed25519VerificationKey2020
 
   describe('X25519KeyAgreementKey2020', () => {
+    let keyId;
+    before(async () => {
+      keyId = `https://example.com/kms/${await generateId()}`;
+    });
     describe('generateKey API', () => {
       it('successfully generates a key pair', async () => {
         // uuid will be generated at the bedrock-kms-http layer
-        const keyId = `https://example.com/kms/${await generateId()}`;
         const controller = 'https://example.com/i/foo';
         const type = 'X25519KeyAgreementKey2020';
         const invocationTarget = {id: keyId, type, controller};
@@ -140,6 +302,30 @@ describe('asymmetric keys', () => {
         result.id.should.equal(keyId);
         result.type.should.equal(type);
       });
+      it('throws a DuplicateError when generating the same key twice',
+        async () => {
+          // keyId will be generated at the bedrock-kms-http layer
+          const keyId = `https://example.com/kms/${await generateId()}`;
+          const controller = 'https://example.com/i/foo';
+          const type = 'X25519KeyAgreementKey2020';
+          const invocationTarget = {id: keyId, type, controller};
+          await brSSM.generateKey(
+            {keyId, operation: {invocationTarget}});
+
+          let result;
+          let err;
+          try {
+            result = await brSSM.generateKey(
+              {keyId, operation: {invocationTarget}});
+            should.exist(result);
+          } catch(e) {
+            err = e;
+          }
+
+          should.exist(err);
+          should.not.exist(result);
+          err.name.should.equal('DuplicateError');
+        });
     }); // end generateKey API
 
     describe('deriveSecret API', () => {
@@ -161,5 +347,113 @@ describe('asymmetric keys', () => {
         secret.should.be.a('string');
       });
     }); // end sign API
+
+    describe('getKeyDescription API', () => {
+      it('returns a key description for keyId with publicKeyMultibase',
+        async () => {
+          const keyId = `https://example.com/kms/${await generateId()}`;
+          const controller = 'https://example.com/i/foo';
+          const type = 'X25519KeyAgreementKey2020';
+          const invocationTarget = {id: keyId, type, controller};
+          await brSSM.generateKey(
+            {keyId, operation: {invocationTarget}});
+
+          const result = await brSSM.getKeyDescription({keyId});
+          result.should.be.an('object');
+          result.should.have.property('id', keyId);
+          result.should.have.property('type', 'X25519KeyAgreementKey2020');
+          result.should.have.property('@context');
+          result['@context'].should.equal(
+            'https://w3id.org/security/suites/x25519-2020/v1');
+          result.should.have.property('publicKeyMultibase');
+        });
+    });
+
+    describe('wrapKey API', () => {
+      it('throws an Error when trying to wrap a key',
+        async () => {
+          const unwrappedKey = '8vEgpnq8F6QVRmaSYPHTKKZyCXMOgRLiBdZPcfYnIfI';
+
+          let result;
+          let err;
+          try {
+            result = await brSSM.wrapKey(
+              {keyId, operation: {unwrappedKey}});
+          } catch(e) {
+            err = e;
+          }
+
+          should.exist(err);
+          should.not.exist(result);
+          err.name.should.equal('Error');
+        });
+    });
+    describe('unwrapKey API', () => {
+      it('throws an Error when trying to unwrap a key',
+        async () => {
+          const wrappedKey = '8vEgpnq8F6QVRmaSYPHTKKZyCXMOgRLiBdZPcfYnIfI';
+
+          let result;
+          let err;
+          try {
+            result = await brSSM.unwrapKey(
+              {keyId, operation: {wrappedKey}});
+          } catch(e) {
+            err = e;
+          }
+
+          should.exist(err);
+          should.not.exist(result);
+          err.name.should.equal('Error');
+        });
+    });
+    describe('sign API', () => {
+      it('throws an Error when trying to sign', async () => {
+        const keyId = `https://example.com/kms/${await generateId()}`;
+        const controller = 'https://example.com/i/foo';
+        const type = 'X25519KeyAgreementKey2020';
+        const invocationTarget = {id: keyId, type, controller};
+        const result = await brSSM.generateKey(
+          {keyId, operation: {invocationTarget}});
+        should.exist(result);
+        const plaintextBuffer = Buffer.from(uuid(), 'utf8');
+        const verifyData = base64url.encode(plaintextBuffer);
+
+        let signResult;
+        let err;
+        try {
+          signResult = await brSSM.sign(
+            {keyId, operation: {invocationTarget, verifyData}});
+        } catch(e) {
+          err = e;
+        }
+
+        should.exist(err);
+        should.not.exist(signResult);
+        err.name.should.equal('Error');
+      });
+    }); // end sign API
+
+    describe('verify API', () => {
+      it('throws an Error when trying to verify',
+        async () => {
+          const plaintextBuffer = Buffer.from(uuid(), 'utf8');
+          const verifyData = base64url.encode(plaintextBuffer);
+          const signatureValue = 'test';
+
+          let result;
+          let err;
+          try {
+            result = await brSSM.verify(
+              {keyId, operation: {signatureValue, verifyData}});
+          } catch(e) {
+            err = e;
+          }
+
+          should.exist(err);
+          should.not.exist(result);
+          err.name.should.equal('Error');
+        });
+    });
   });
 });
