7b04e662e757492265314a9df374f02310da7579
Dave Longley <dlongley@digitalbazaar.com>
2025-02-03 17:20:27 -0500

    Finish key record encryption feature and enable tests.

--- test/mocha/helpers.js
+++ test/mocha/helpers.js
@@ -1,5 +1,5 @@
 /*!
- * Copyright (c) 2021-2022 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2021-2025 Digital Bazaar, Inc. All rights reserved.
  */
 export function localId({id}) {
   const idx = id.lastIndexOf('/');
